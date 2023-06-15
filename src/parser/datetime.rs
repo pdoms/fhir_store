038fd6d40eedf342f793f504d7746101d93cf9b1
pdoms <domspaulo@gmail.com>
2023-06-15 20:06:57 +0200

    Fhir_DateTime::from_timestamp

--- src/parser/datetime.rs
+++ src/parser/datetime.rs
@@ -1,5 +1,5 @@
 use crate::error::{Error, Result};
-use chrono::{DateTime, Utc, naive::Days, Months};
+use chrono::{DateTime, Utc, naive::Days, Months, NaiveDateTime, FixedOffset};
 
 //From https://build.fhir.org/datatypes.html#dateTime
 //YYYY                          2018          
@@ -7,14 +7,12 @@ use chrono::{DateTime, Utc, naive::Days, Months};
 //YYYY-MM-DD                    1905-08-23
 //YYYY-MM-DDThh:mm:ss+zz:zz     2015-02-07T13:28:17-05:00   2017-01-01T00:00:00.000Z
 
-
-
 //TODO: TIMEZONE!!!!
+//it is always saved in utc time
 #[derive(Default)]
 pub struct Fhir_DateTime {
     c: DateTime<Utc>,
 }
-
 
 
 
@@ -59,10 +57,22 @@ impl Fhir_DateTime {
             }
         }
     }
-    pub fn from_timestamp<T>(ts: T) {
-        todo!()
+    pub fn from_timestamp_millis(ts: i64) -> Result<Self> {
+        if let Some(ndt) = NaiveDateTime::from_timestamp_millis(ts) {
+            let dt = DateTime::<Utc>::from_utc(ndt, Utc);
+            Ok(Self { c: dt })
+        } else {
+            Err(Error::TimeStampParsingError)
+        }  
     }
-    pub fn from_timestamp_bytes() {}
+
+    pub fn from_timestamp_bytes(ts: &[u8]) -> Result<Self> {
+        let bytes: [u8; 8] = match ts.try_into() {
+            Ok(v) => v,
+            Err(_) => {return Err(Error::TimeStampParsingError)}
+        };
+        Fhir_DateTime::from_timestamp_millis(i64::from_be_bytes(bytes))
+    }
 
     pub fn timestamp(&self) -> i64 {
         self.c.timestamp()
@@ -92,6 +102,7 @@ fn to_u32(s: &str) -> Result<u32> {
         Err(_) => Err(Error::TimeStampParsingError)
     }
 }
+
 
 
 
@@ -135,6 +146,15 @@ mod test {
         let conv_2 = Fhir_DateTime::from_string("2017-01-01T00:00:00.000Z").unwrap();
         assert_eq!(conv_1.timestamp(),1423333697);
         assert_eq!(conv_2.timestamp_millis(),1483228800000);
+    }
+
+    #[test]
+    fn fhir_dt_from_timestamp() {
+        let ts = 1483228800000i64;
+        let conv = Fhir_DateTime::from_timestamp_millis(ts).unwrap();
+        assert_eq!(conv.c.to_rfc3339_opts(chrono::SecondsFormat::Millis, true), "2017-01-01T00:00:00.000Z".to_string());
+        let conv_2 = Fhir_DateTime::from_timestamp_bytes(&ts.to_be_bytes()).unwrap();
+        assert_eq!(conv_2.c.to_rfc3339_opts(chrono::SecondsFormat::Millis, true), "2017-01-01T00:00:00.000Z".to_string());
     }
 }
 
